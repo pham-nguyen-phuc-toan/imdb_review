ce385eaa4b4fa46ef3d47c4348ba898e09b9cccd
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-12-16 20:43:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from PIL import Image
 import pickle as pkl
 import tensorflow as tf
 from tensorflow import keras
+from keras.datasets import imdb
 import numpy as np
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+maxlen = 200
+
+index = imdb.get_word_index()
 
 class_list = {'0': 'Negative', '1': 'Positve'}
 
@@ -20,7 +26,11 @@ txt = st.text_area('', '')
 
 if txt != '':
     if st.button('Predict'):
-        feature_vector = np.expand_dims([txt], axis=2)
+        x_test = []
+        for w in txt.split():
+            x_test += [index[w]]        
+        x_test = pad_sequences(x_test, maxlen=maxlen)
+        feature_vector = np.expand_dims(x_test, axis=2)
         label = str((model.predict(feature_vector))[0])
 
         st.header('Result')
